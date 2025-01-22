e75f8dc84f965683ca5daa05abb30a776a7ba448
Unehh <pawelwrobel06092002@gmail.com>
2025-01-22 15:33:36 +0100

    new Calculator

--- src/main/java/org/example/CalculatorAction.java
+++ src/main/java/org/example/CalculatorAction.java
@@ -8,11 +8,30 @@ import static org.example.Calculator.trimmer;
 
 public class CalculatorAction extends Action{
     private static String numberVal = "", numberSaved = "", NumberAfterCalculations = "";
-    static Character CalcCharacter = '\0'; //Character is Empty
+    static Character calcCharacter = '\0'; //Character is Empty
     boolean isNegative = false, isFloat = false;
     private String action;
     private String buttonVal;
     private JLabel label;
+    private static String Calculate(String FirstNumber, String SecondNumber, Character calcCharacter) {
+        String calculated = "0";
+        switch (calcCharacter){
+            case '+':
+                calculated = String.valueOf(Double.valueOf(FirstNumber)+Double.valueOf(SecondNumber));
+                break;
+            case '-':
+                calculated = String.valueOf(Double.valueOf(FirstNumber)-Double.valueOf(SecondNumber));
+                break;
+            case 'x':
+                calculated = String.valueOf(Double.valueOf(FirstNumber)*Double.valueOf(SecondNumber));
+                break;
+            case '/':
+                calculated = String.valueOf(Double.valueOf(FirstNumber)/Double.valueOf(SecondNumber));
+                break;
+        }
+        calculated = trimmer(calculated);
+        return calculated;
+    }
     CalculatorAction(String buttonVal, JFrame frame, GridBagConstraints gridConstraint, int gridX, int gridY, String action, JLabel label) {
         super(buttonVal, frame, gridConstraint, gridX, gridY);
         this.action = action;
@@ -31,8 +50,9 @@ public class CalculatorAction extends Action{
             case "Add Number":
                 if(!NumberAfterCalculations.isEmpty()) {
                     numberSaved = "";
-                    CalcCharacter = '\0';
+                    calcCharacter = '\0';
                     numberVal = "";
+                    NumberAfterCalculations = "";
                 }
                 if (numberVal.equals("0") || numberVal.isEmpty()) {
                     numberVal = buttonVal;
@@ -51,7 +71,7 @@ public class CalculatorAction extends Action{
             case "Clear Entry":
                 if (numberVal == "") {
                     numberSaved = "";
-                    CalcCharacter = '\0';
+                    calcCharacter = '\0';
                     numberVal = "0";
                 } else {
                     numberVal = "0";
@@ -60,7 +80,7 @@ public class CalculatorAction extends Action{
             case "Clear":
                 numberVal = "0";
                 numberSaved = "";
-                CalcCharacter = 'e';
+                calcCharacter = '\0';
                 break;
             case "Decimal Number":
                 if (numberVal == "0") {
@@ -98,7 +118,14 @@ public class CalculatorAction extends Action{
                 numberVal = trimmer(numberVal);
                 break;
             case "Turn into decimal":
+
                 if (!isFloat) {
+                    if(!NumberAfterCalculations.isEmpty()) {
+                        numberSaved = "";
+                        calcCharacter = '\0';
+                        numberVal = "0";
+                        NumberAfterCalculations = "";
+                    }
                     numberVal = numberVal + ".";
                 }
                 break;
@@ -110,16 +137,46 @@ public class CalculatorAction extends Action{
                 }
                 break;
             case "Divide":
-                CalcCharacter = '/';
+                if(numberSaved.isEmpty()) {
+                    numberSaved = numberVal;
+                } else if (numberVal == "0") {
+                    System.out.println("CANNOT DIVIDE BY 0");
+                    break;
+                } else{
+                    numberSaved = Calculate(numberSaved, numberVal, calcCharacter);
+                }
+                calcCharacter = '/';
+                numberVal = "";
                 break;
             case "Multiply":
-                CalcCharacter = 'x';
+                if(numberSaved.isEmpty()) {
+                    numberSaved = numberVal;
+                } else{
+                    numberSaved = Calculate(numberSaved, numberVal, calcCharacter);
+                }
+                calcCharacter = 'x';
+                numberVal = "";
                 break;
             case "Subtraction":
-                CalcCharacter = '-';
+                if(numberSaved.isEmpty()) {
+                    numberSaved = numberVal;
+                } else{
+                    numberSaved = Calculate(numberSaved, numberVal, calcCharacter);
+                }
+                calcCharacter = '-';
+                numberVal = "";
                 break;
             case "Add":
-                CalcCharacter = '+';
+                if(numberSaved.isEmpty()) {
+                    numberSaved = numberVal;
+                } else{
+                    numberSaved = Calculate(numberSaved, numberVal, calcCharacter);
+                }
+                calcCharacter = '+';
+                numberVal = "";
+                break;
+            case "Equals":
+                NumberAfterCalculations = Calculate(numberSaved, numberVal, calcCharacter);
                 break;
                 default:
                 throw new IllegalStateException("Unexpected value: " + action);
@@ -133,9 +190,10 @@ public class CalculatorAction extends Action{
                 isFloat = false;
             }
         }
-        label.setText(numberSaved + CalcCharacter + numberVal);
+        label.setText(numberSaved + calcCharacter + numberVal);
         if(!NumberAfterCalculations.isEmpty()) {
-            label.setText(numberSaved + CalcCharacter + numberVal + "=" + NumberAfterCalculations);
+            label.setText(numberSaved + calcCharacter + numberVal + "=" + NumberAfterCalculations);
+            numberSaved = NumberAfterCalculations;
         }
     }
 }
